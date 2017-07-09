49a40fbd47b6b12dd40422883dfe290903e88cae
Ricky Nelson <rickyn@socketwiz.com>
2017-07-09 17:35:40 -0400

    Update package.json

---
+++ next.config.js
@@ -0,0 +1,43 @@
+
+module.exports = {
+    exportPathMap: function exportPathMap() {
+        return {
+            '/': {
+                'page': '/'
+            },
+            '/about': {
+                'page': '/about'
+            },
+            '/2014-08-03-using-dtrace-to-fix-git.js': {
+                'page': '/2014-08-03-using-dtrace-to-fix-git.js'
+            },
+            '/2014-07-04-github-for-blogging.js': {
+                'page': '/2014-07-04-github-for-blogging.js'
+            },
+            '/2012-08-23-caps-lock.js': {
+                'page': '/2012-08-23-caps-lock.js'
+            },
+            '/2012-05-20-vim-cheat-sheet.js': {
+                'page': '/2012-05-20-vim-cheat-sheet.js'
+            },
+            '/2012-05-08-un-close-apps-with-mac-task-switcher.js': {
+                'page': '/2012-05-08-un-close-apps-with-mac-task-switcher.js'
+            },
+            '/2012-05-07-vi-rediscovered.js': {
+                'page': '/2012-05-07-vi-rediscovered.js'
+            },
+            '/2010-10-04-use-console-app-to-monitor-any-log-file.js': {
+                'page': '/2010-10-04-use-console-app-to-monitor-any-log-file.js'
+            },
+            '/2010-08-02-how-to-extract-an-icon.js': {
+                'page': '/2010-08-02-how-to-extract-an-icon.js'
+            },
+            '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free.js': {
+                'page': '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free.js'
+            },
+            '/2010-07-17-python-via-cocoa.js': {
+                'page': '/2010-07-17-python-via-cocoa.js'
+            }
+        };
+    }
+};
