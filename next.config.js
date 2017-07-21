661b85c40b9aea0bedb6a0af405a3ec88463a9f4
Ricky Nelson <rickyn@socketwiz.com>
2017-07-20 22:39:35 -0400

    fixing 404 pages

--- next.config.js
+++ next.config.js
@@ -8,38 +8,38 @@ module.exports = {
             '/about': {
                 'page': '/about'
             },
-            '/2017-07-10-from-vim-to-spacemacs-to-emacs.js': {
-                'page': '/2017-07-10-from-vim-to-spacemacs-to-emacs.js'
+            '/2017-07-10-from-vim-to-spacemacs-to-emacs': {
+                'page': '/2017-07-10-from-vim-to-spacemacs-to-emacs'
             },
-            '/2014-08-03-using-dtrace-to-fix-git.js': {
-                'page': '/2014-08-03-using-dtrace-to-fix-git.js'
+            '/2014-08-03-using-dtrace-to-fix-git': {
+                'page': '/2014-08-03-using-dtrace-to-fix-git'
             },
-            '/2014-07-04-github-for-blogging.js': {
-                'page': '/2014-07-04-github-for-blogging.js'
+            '/2014-07-04-github-for-blogging': {
+                'page': '/2014-07-04-github-for-blogging'
             },
-            '/2012-08-23-caps-lock.js': {
-                'page': '/2012-08-23-caps-lock.js'
+            '/2012-08-23-caps-lock': {
+                'page': '/2012-08-23-caps-lock'
             },
-            '/2012-05-20-vim-cheat-sheet.js': {
-                'page': '/2012-05-20-vim-cheat-sheet.js'
+            '/2012-05-20-vim-cheat-sheet': {
+                'page': '/2012-05-20-vim-cheat-sheet'
             },
-            '/2012-05-08-un-close-apps-with-mac-task-switcher.js': {
-                'page': '/2012-05-08-un-close-apps-with-mac-task-switcher.js'
+            '/2012-05-08-un-close-apps-with-mac-task-switcher': {
+                'page': '/2012-05-08-un-close-apps-with-mac-task-switcher'
             },
-            '/2012-05-07-vi-rediscovered.js': {
-                'page': '/2012-05-07-vi-rediscovered.js'
+            '/2012-05-07-vi-rediscovered': {
+                'page': '/2012-05-07-vi-rediscovered'
             },
-            '/2010-10-04-use-console-app-to-monitor-any-log-file.js': {
-                'page': '/2010-10-04-use-console-app-to-monitor-any-log-file.js'
+            '/2010-10-04-use-console-app-to-monitor-any-log-file': {
+                'page': '/2010-10-04-use-console-app-to-monitor-any-log-file'
             },
-            '/2010-08-02-how-to-extract-an-icon.js': {
-                'page': '/2010-08-02-how-to-extract-an-icon.js'
+            '/2010-08-02-how-to-extract-an-icon': {
+                'page': '/2010-08-02-how-to-extract-an-icon'
             },
-            '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free.js': {
-                'page': '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free.js'
+            '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free': {
+                'page': '/2010-07-21-how-to-recover-data-from-a-memory-card-for-free'
             },
-            '/2010-07-17-python-via-cocoa.js': {
-                'page': '/2010-07-17-python-via-cocoa.js'
+            '/2010-07-17-python-via-cocoa': {
+                'page': '/2010-07-17-python-via-cocoa'
             }
         };
     }
