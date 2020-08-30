05b8f91bcd117b3b16b022ad0fe3325cbba61c60
Ricky Nelson <rickyn@socketwiz.com>
2020-08-30 16:18:15 -0400

    security updates

--- out/_next/f3fdce2c-b316-44de-9e67-d5bf276b2aa2/page/2012-08-23-caps-lock/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/2012-08-23-caps-lock', function() {
-            var comp = module.exports=webpackJsonp([10],{353:function(e,t,a){e.exports=a(354)},354:function(e,t,a){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=a(6),u=l(n),o=a(1),r=l(o),c=a(2),d=l(c),f=a(7),s=l(f),p=a(8),i=l(p),m=a(3),h=l(m),k=a(25),y=l(k),_=a(16),E=l(_),g=a(26),v=l(g),C=function(e){function t(){return(0,r.default)(this,t),(0,s.default)(this,(t.__proto__||(0,u.default)(t)).apply(this,arguments))}return(0,i.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return h.default.createElement(E.default,{title:"Caps Lock"},h.default.createElement("h2",null,"Caps Lock"),h.default.createElement(v.default,{posted:"23 Aug 2012"}),h.default.createElement("p",null,"To all my VI and Terminal users out there…Your not using your caps lock anyway, remap it to control and keep that pinky on the home row and save yourself some carpal tunnel, hehe:"),h.default.createElement("p",null,h.default.createElement("img",{src:"/static/images/caps_lock.png",alt:"Caps Lock"})),h.default.createElement(y.default,null))}}]),t}(m.Component);t.default=C}},[353]);
-            return { page: comp.default }
-          })
-        
