51041cb2db0d6222493ffc5f03df1083a0c4d9d0
Ricky Nelson <rickyn@socketwiz.com>
2021-12-01 08:28:28 -0500

    converting to zola, removing blog

---
+++ content/index.md
@@ -0,0 +1,5 @@
++++
+date = 2018-02-24
+title = "Starting a new blog"
++++
+foo
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+        "./templates/**/*.html"
+    ],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        // ...
+        require('@tailwindcss/forms'),
+    ],
+}
